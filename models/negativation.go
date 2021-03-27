afc0f9f1e61a2c34340b254031690ca84df7dda6
rafaelsanzio <rafaelsanzio27@gmail.com>
2021-03-27 17:47:31 -0300

    feat: creating model negativation
    feat: creating model negativation

--- models/negativation.go
+++ models/negativation.go
@@ -4,7 +4,7 @@ type Negativation struct {
 	CompanyDocument   string        `bson:"companyDocument" json:"companyDocument"`
 	CompanyName       string        `bson:"companyName" json:"companyName"`
 	CustomerDocument  string        `bson:"customerDocument" json:"customerDocument"`
-	Value             int           `bson:"value" json:"value"`
+  Value             float64       `bson:"value" json:"value"`
 	Contract          string        `bson:"contract" json:"contract"`
 	DebtDate          string        `bson:"debtDate" json:"debtDate"`
 	InclusionDate     string        `bson:"inclusionDate" json:"inclusionDate"`
@@ -13,5 +13,6 @@ type Negativation struct {
 
 type FilterNegativation struct {
 	CompanyName      *string
+	CompanyDocument  *string
 	CustomerDocument *string
 }
